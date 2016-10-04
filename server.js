11557538ce5298c609a7cce8d9b9760d2bbbce10
Raphael Constantino <raphaelc23@gmail.com>
2016-10-04 10:59:54 +0000

    Adding project

--- server.js
+++ server.js
@@ -1,10 +1,65 @@
 var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var moment = require('moment');
+
+var port = process.env.PORT || 8080;
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.listen(port, function() {
+  console.log("Server up listening to: " + port);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/', function(req, res) {
+  var fileName = path.join(__dirname, 'index.html');
+  
+  res.sendFile(fileName, function (err) {
+    
+    if (err) 
+    {
+      console.log(err);
+      res.status(err.status).end();
+      return;
+    }
+
+    console.log('Sent:', fileName);
+    
+  });
+});
+
+app.get('/:datestring', function(req, res) {
+  var date;
+  
+  if(/^\d{8,}$/.test(req.params.datestring)) 
+  {
+    
+    date = moment(req.params.datestring, "X");
+    
+  } else 
+  {
+    
+    date = moment(req.params.datestring, "MMMM D, YYYY");
+    
+  }
+
+  if(date.isValid()) 
+  {
+    
+    res.json({
+      unix: date.format("X"),
+      natural: date.format("MMMM D, YYYY")
+    });
+    
+  } else 
+  {
+    
+    res.json({
+      unix: null,
+      natural: null
+    });
+    
+  }
+
+
 });
